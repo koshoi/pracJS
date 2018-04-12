94670d13d51a0243bf92298f5dcd1973c3143f42
Oleg Koshovets <okoshovetc@gmail.com>
2018-04-12 08:50:35 +0300

    Initial commit

---
+++ js/supermarket.js
@@ -0,0 +1,12 @@
+class SuperMarket {
+	// default constructor that initializes all the basic fields
+	constructor() {
+		this.MaxQueueLenght     = 7
+		this.MaxCashBoxes       = 5
+		this.CommercialExpenses = 0
+		this.Discount           = 0
+		this.Balance            = 0
+		this.CashierSalary      = 2000
+		this.CashBoxes          = new Array()
+	}
+}
